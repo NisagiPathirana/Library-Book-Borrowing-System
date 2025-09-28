72fcc4182cacbc8ecf440b0836f726e7f77e9891
NisagiPathirana <kpndomalsa19@gmail.com>
2025-09-28 09:47:09 +0530

    create the model package and add Book class

---
+++ src/main/java/edu/icet/model/Book.java
@@ -0,0 +1,13 @@
+package edu.icet.model;
+
+
+public class Book {
+    private long Id;
+    private String title;
+    private String author;
+    private String publisher;
+    private String isbn;
+    private String category;
+    private String availableCopies;
+
+}
